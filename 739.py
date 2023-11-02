9b3be9284f137592b0fd94ceb75b6c454c317c53
Vansh Nagpal <2002vansh@gmail.com>
2023-11-02 14:08:16 -0400

    Daily Temperatures

--- 739.py
+++ 739.py
@@ -3,14 +3,16 @@ class Solution:
         # fill with 0 as default values
         to_return = [0] * len(temperatures)
         
-        # contains a backlog of seen temperatures, will only pop off an element if we encounter a greater element than the top
+        # contains a backlog of seen temperatures, 
+        # will only pop off an element if we encounter a greater element than the top
         temp_stack = [] 
 
         # iterate over the list once
         for i, temp in enumerate(temperatures):
             # first checks stack is non empty
             # if stack is empty skips and appends new value
-            # continues through while loop until we encounter a value in the stack that is >= than the current value
+            # continues through while loop until 
+            # we encounter a value in the stack that is >= than the current value
             while temp_stack and temp > temp_stack[-1][0]:
                 # in to return list, replace the default 0 as the difference between the two indices
                 to_return[temp_stack[-1][1]] = i - temp_stack[-1][1]
